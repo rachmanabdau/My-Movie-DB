e473f72b05d8f5d945b7b4bd230599ba384bdc58
Rachman Arif Abda'u <rachmanabdau@gmail.com>
2020-10-19 12:56:13 +0700

    Create Login as a Guest Token Model

---
+++ app/src/main/java/com/example/mymoviddb/model/GuestSessionModel.kt
@@ -0,0 +1,13 @@
+package com.example.mymoviddb.model
+
+
+import com.squareup.moshi.Json
+
+data class GuestSessionModel(
+    @Json(name = "expires_at")
+    val expiresAt: String,
+    @Json(name = "guest_session_id")
+    val guestSessionId: String,
+    @Json(name = "success")
+    val success: Boolean
+)
