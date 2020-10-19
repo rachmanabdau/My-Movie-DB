3778285aec113361b42540fe692c097c2ebbc742
Rachman Arif Abda'u <rachmanabdau@gmail.com>
2020-10-19 12:55:34 +0700

    Create Login Token Model

---
+++ app/src/main/java/com/example/mymoviddb/model/LoginTokenModel.kt
@@ -0,0 +1,11 @@
+package com.example.mymoviddb.model
+
+import com.squareup.moshi.Json
+
+data class LoginTokenModel(
+    val success: Boolean,
+    @Json(name = "expires_at")
+    val expiresAt: String,
+    @Json(name = "request_token")
+    val requestToken: String
+)
