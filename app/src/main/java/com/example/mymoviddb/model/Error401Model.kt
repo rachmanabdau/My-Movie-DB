6adb2b522da187feab6c55b868170fceb214a027
Rachman Arif Abda'u <rachmanabdau@gmail.com>
2020-10-19 12:56:53 +0700

    Create Model to Handle Error 401

---
+++ app/src/main/java/com/example/mymoviddb/model/Error401Model.kt
@@ -0,0 +1,13 @@
+package com.example.mymoviddb.model
+
+
+import com.squareup.moshi.Json
+
+data class Error401Model(
+    @Json(name = "status_code")
+    val statusCode: Int,
+    @Json(name = "status_message")
+    val statusMessage: String,
+    @Json(name = "success")
+    val success: Boolean
+)
