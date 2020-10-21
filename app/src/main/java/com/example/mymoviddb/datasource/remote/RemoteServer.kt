9a455e4026266f4724a01630dc63d54dac623563
Rachman Arif Abda'u <rachmanabdau@gmail.com>
2020-10-21 10:23:28 +0700

    Create Interface Remote Server

---
+++ app/src/main/java/com/example/mymoviddb/datasource/remote/RemoteServer.kt
@@ -0,0 +1,19 @@
+package com.example.mymoviddb.datasource.remote
+
+import com.example.mymoviddb.model.GuestSessionModel
+import com.example.mymoviddb.model.LoginTokenModel
+import com.example.mymoviddb.model.RequestTokenModel
+import com.example.mymoviddb.model.Result
+
+interface RemoteServer {
+
+    suspend fun requestAccessToken(): Result<RequestTokenModel?>
+
+    suspend fun loginAsUser(
+        username: String,
+        password: String,
+        requestToken: RequestTokenModel
+    ): Result<LoginTokenModel?>
+
+    suspend fun loginAsGuest(): Result<GuestSessionModel?>
+}
