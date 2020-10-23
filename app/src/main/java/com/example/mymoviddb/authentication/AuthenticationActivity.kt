92ff70f9ec01c9557629d826358ded760593a2eb
Rachman Arif Abda'u <rachmanabdau@gmail.com>
2020-10-23 17:11:16 +0700

    Attach Navigation Host to Authentication Activity

--- app/src/main/java/com/example/mymoviddb/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/example/mymoviddb/authentication/AuthenticationActivity.kt
@@ -3,8 +3,11 @@ package com.example.mymoviddb.authentication
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.example.mymoviddb.R
 import com.example.mymoviddb.databinding.ActivityAuthenticationBinding
+
 
 class AuthenticationActivity : AppCompatActivity() {
 
@@ -13,5 +16,10 @@ class AuthenticationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_authentication)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
 }
