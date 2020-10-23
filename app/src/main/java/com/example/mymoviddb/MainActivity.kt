f6c8bb0ca34447beac498ba420b1d0e04d2fcc5f
Rachman Arif Abda'u <rachmanabdau@gmail.com>
2020-10-23 19:40:46 +0700

    Add Navigation Host Fragment to Main Activity

--- app/src/main/java/com/example/mymoviddb/MainActivity.kt
+++ app/src/main/java/com/example/mymoviddb/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.mymoviddb
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.example.mymoviddb.databinding.ActivityAuthenticationBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAuthenticationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.main_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        //without drawer menu or appbar configuration
+        return Navigation.findNavController(this, R.id.main_host_fragment).navigateUp()
+                || super.onSupportNavigateUp()
     }
 }
