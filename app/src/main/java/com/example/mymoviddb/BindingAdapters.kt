85abe8d1ad6c830461db2b974bbfe2af51087116
Rachman Arif Abda'u <rachmanabdau@gmail.com>
2020-10-23 14:50:57 +0700

    Create Binding Adapters for Progressbar

---
+++ app/src/main/java/com/example/mymoviddb/BindingAdapters.kt
@@ -0,0 +1,25 @@
+package com.example.mymoviddb
+
+import android.view.View
+import android.widget.ProgressBar
+import androidx.databinding.BindingAdapter
+import com.example.mymoviddb.model.Result
+import com.google.android.material.snackbar.Snackbar
+
+@BindingAdapter("showLoadingState")
+fun showProgressbar(view: ProgressBar, result: Result<*>?) {
+    view.visibility = when (result) {
+        is Result.Loading -> View.VISIBLE
+        is Result.Success -> {
+            View.GONE
+        }
+        is Result.Error -> {
+            val errorMessage = result.exception.localizedMessage as String
+            Snackbar.make(view, errorMessage, Snackbar.LENGTH_LONG).show()
+            View.GONE
+        }
+        null -> {
+            View.GONE
+        }
+    }
+}
