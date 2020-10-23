10aa6de6b718b14f7cc9d264b9a985a45c5d7cbb
Rachman Arif Abda'u <rachmanabdau@gmail.com>
2020-10-23 15:00:50 +0700

    Create Authentication Fragment

--- app/src/main/java/com/example/mymoviddb/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/example/mymoviddb/authentication/AuthenticationActivity.kt
@@ -1,27 +1,17 @@
 package com.example.mymoviddb.authentication
 
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.mymoviddb.R
 import com.example.mymoviddb.databinding.ActivityAuthenticationBinding
-import com.example.mymoviddb.datasource.remote.RemoteServerAccess
 
 class AuthenticationActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAuthenticationBinding
 
-    private val authenticationViewModel by viewModels<AuthenticationViewModel> {
-        val remoteServer = RemoteServerAccess()
-        AuthenticationViewModel.Facroty(remoteServer)
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_authentication)
-
-        binding.lifecycleOwner = this
-        binding.authenticationVM = authenticationViewModel
     }
 }
---
+++ app/src/main/java/com/example/mymoviddb/authentication/AuthenticationFragment.kt
@@ -0,0 +1,33 @@
+package com.example.mymoviddb.authentication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.mymoviddb.databinding.FragmentAuthenticationBinding
+import com.example.mymoviddb.datasource.remote.RemoteServerAccess
+
+class AuthenticationFragment : Fragment() {
+
+    private lateinit var binding: FragmentAuthenticationBinding
+
+    private val authenticationViewModel by viewModels<AuthenticationViewModel> {
+        val remoteServer = RemoteServerAccess()
+        AuthenticationViewModel.Facroty(remoteServer)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentAuthenticationBinding.inflate(inflater, container, false)
+
+        binding.lifecycleOwner = this
+        binding.authenticationVM = authenticationViewModel
+
+        return binding.root
+    }
+
+}
