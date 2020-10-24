5e11bb2204e981dfb4d7e8a7a8f81ed889e824ee
Rachman Arif Abda'u <rachmanabdau@gmail.com>
2020-10-24 18:04:27 +0700

    Create Authstate Preference

--- app/src/main/java/com/example/mymoviddb/utils/PreferenceUtil.kt
+++ app/src/main/java/com/example/mymoviddb/utils/PreferenceUtil.kt
@@ -3,9 +3,25 @@ package com.example.mymoviddb.utils
 import android.app.Activity
 import android.content.Context
 
+
 object PreferenceUtil {
 
-    private val GUEST_TOKEN_KEY = "package com.example.mymoviddb.utils.PreferenceUtil.tokenKey"
+    private val GUEST_TOKEN_KEY = "package com.example.mymoviddb.utils.PreferenceUtil.guesttokenKey"
+    private val LOGIN_STATE_KEY = "package com.example.mymoviddb.utils.PreferenceUtil.loginStateKey"
+
+    fun setAuthState(activity: Activity?, state: LoginState) {
+        val loginStatePref = activity?.getPreferences(Context.MODE_PRIVATE) ?: return
+        with(loginStatePref.edit()) {
+            putInt(LOGIN_STATE_KEY, state.ordinal)
+            commit()
+        }
+    }
+
+    fun setAuthState(activity: Activity?): Int {
+        val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE)
+
+        return sharedPref?.getInt(LOGIN_STATE_KEY, 0) ?: 0
+    }
 
     fun writeGuestToken(activity: Activity?, token: String) {
         val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE) ?: return
@@ -21,4 +37,10 @@ object PreferenceUtil {
 
         return sharedPref?.getString(GUEST_TOKEN_KEY, "") ?: ""
     }
+}
+
+enum class LoginState(stateId: Int) {
+    NOT_LOG_IN(0),
+    AS_GUEST(1),
+    AS_USER(2)
 }
