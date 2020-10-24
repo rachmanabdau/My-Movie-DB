d9f474f671a72209416f6598913adf4c28f6dbfd
Rachman Arif Abda'u <rachmanabdau@gmail.com>
2020-10-24 14:44:11 +0700

    Create Binding Adapter for Popular Movie Recycler View

--- app/src/main/java/com/example/mymoviddb/utils/BindingAdapters.kt
+++ app/src/main/java/com/example/mymoviddb/utils/BindingAdapters.kt
@@ -5,10 +5,13 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.ProgressBar
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
 import com.example.mymoviddb.BuildConfig
+import com.example.mymoviddb.adapters.MoviesAdapter
+import com.example.mymoviddb.model.MovieModel
 import com.example.mymoviddb.model.Result
 import com.google.android.material.snackbar.Snackbar
 
@@ -17,22 +20,18 @@ import com.google.android.material.snackbar.Snackbar
 fun showProgressbar(view: ProgressBar, result: Result<*>?) {
     view.visibility = when (result) {
         is Result.Loading -> View.VISIBLE
-        is Result.Success -> {
-            View.GONE
-        }
         is Result.Error -> {
             val errorMessage = result.exception.localizedMessage as String
             Snackbar.make(view, errorMessage, Snackbar.LENGTH_LONG).show()
             View.GONE
         }
-        null -> {
+        else -> {
             View.GONE
         }
     }
 }
 
 @BindingAdapter("loadImage", "error")
-
 fun loadImage(imageView: ImageView, uri: String?, error: Drawable) {
     val options: RequestOptions = RequestOptions()
         .centerCrop()
@@ -43,4 +42,12 @@ fun loadImage(imageView: ImageView, uri: String?, error: Drawable) {
         .load(BuildConfig.LOAD_POSTER_BASE_URL + uri)
         .apply(options)
         .into(imageView)
+}
+
+@BindingAdapter("attachList")
+fun attachMovieList(recyclerView: RecyclerView, movieList: Result<MovieModel>?) {
+    if (movieList is Result.Success && !movieList.data.results.isNullOrEmpty()) {
+        val adapter = recyclerView.adapter as MoviesAdapter
+        adapter.submitList(movieList.data.results)
+    }
 }
