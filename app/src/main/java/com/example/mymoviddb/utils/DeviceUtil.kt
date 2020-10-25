7eb7a3d10c4550ba6da8c33ec0451097668121cc
Rachman Arif Abda'u <rachmanabdau@gmail.com>
2020-10-25 11:57:56 +0700

    Creaate Object to take Device Screen Dimension

---
+++ app/src/main/java/com/example/mymoviddb/utils/DeviceUtil.kt
@@ -0,0 +1,62 @@
+package com.example.mymoviddb.utils
+
+import android.content.Context
+import android.graphics.Point
+import android.view.WindowInsets
+import android.view.WindowManager
+
+
+/**
+ * Object used to get width or height screen so linear layout can
+ * prefetch loading image
+ */
+object DeviceUtils {
+    /**
+     * @param context
+     * @return the screen height in pixels
+     */
+    fun getScreenHeight(context: Context): Int {
+        val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+
+        return if (android.os.Build.VERSION.SDK_INT < 30) {
+            val display = windowManager.defaultDisplay
+            val size = Point()
+            display.getSize(size)
+            size.y
+        } else {
+            val metrics = windowManager.currentWindowMetrics
+            // Gets all excluding insets
+            val windowInsets = metrics.windowInsets
+            val insets = windowInsets.getInsetsIgnoringVisibility(
+                WindowInsets.Type.navigationBars()
+                        or WindowInsets.Type.displayCutout()
+            )
+
+            insets.top + insets.bottom
+        }
+    }
+
+    /**
+     * @param context
+     * @return the screen width in pixels
+     */
+    fun getScreenWidth(context: Context): Int {
+        val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+
+        return if (android.os.Build.VERSION.SDK_INT < 30) {
+            val display = windowManager.defaultDisplay
+            val size = Point()
+            display.getSize(size)
+            size.x
+        } else {
+            val metrics = windowManager.currentWindowMetrics
+            // Gets all excluding insets
+            val windowInsets = metrics.windowInsets
+            val insets = windowInsets.getInsetsIgnoringVisibility(
+                WindowInsets.Type.navigationBars()
+                        or WindowInsets.Type.displayCutout()
+            )
+            insets.right + insets.left
+        }
+    }
+}
