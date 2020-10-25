ab5f5d7a744808c39a4a0305adb64ad5de9efdf4
Rachman Arif Abda'u <rachmanabdau@gmail.com>
2020-10-25 10:14:26 +0700

    Add Method to Write Preference Util and Check Token Condition

--- app/src/main/java/com/example/mymoviddb/authentication/AuthenticationViewModel.kt
+++ app/src/main/java/com/example/mymoviddb/authentication/AuthenticationViewModel.kt
@@ -1,44 +1,80 @@
 package com.example.mymoviddb.authentication
 
+import android.app.Application
 import androidx.lifecycle.*
 import com.example.mymoviddb.BuildConfig
 import com.example.mymoviddb.datasource.remote.RemoteServer
 import com.example.mymoviddb.model.GuestSessionModel
 import com.example.mymoviddb.model.Result
 import com.example.mymoviddb.utils.Event
-import kotlinx.coroutines.delay
+import com.example.mymoviddb.utils.PreferenceUtil
+import com.example.mymoviddb.utils.Util
 import kotlinx.coroutines.launch
 
-class AuthenticationViewModel(private val remoteSource: RemoteServer) : ViewModel() {
+class AuthenticationViewModel(
+    private val app: Application,
+    private val remoteSource: RemoteServer
+) : ViewModel() {
 
     private val _loginGuestResult = MutableLiveData<Event<Result<GuestSessionModel?>>>()
     val loginGuestResult: LiveData<Event<Result<GuestSessionModel?>>> = _loginGuestResult
 
-    private val _isButtonEnabled = MutableLiveData<Boolean>()
-    val isButtonEnabled: LiveData<Boolean> = _isButtonEnabled
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
 
     init {
-        _isButtonEnabled.value = true
+        _isLoading.value = false
     }
 
     @JvmOverloads
-    fun loginAsGuest(apiKey: String = BuildConfig.V3_AUTH) {
+    fun loginAsGuest(usePreferenceManager: Boolean = true, apiKey: String = BuildConfig.V3_AUTH) {
         viewModelScope.launch {
-            _loginGuestResult.value = Event(Result.Loading)
-            _isButtonEnabled.value = false
-            delay(2000)
-            _loginGuestResult.value = Event(remoteSource.loginAsGuest(apiKey))
-            _isButtonEnabled.value = true
+            _isLoading.value = true
+            if (isTokenValidAndAvailable()) {
+                val result = Event(remoteSource.loginAsGuest(apiKey))
+                _loginGuestResult.value = result
+
+                if (usePreferenceManager && result.peekContent() is Result.Success) {
+                    val restultContent =
+                        (result.peekContent() as Result.Success<GuestSessionModel?>)
+                            .data as GuestSessionModel
+                    saveTokenAndExpiry(restultContent)
+                }
+            } else {
+                val token = PreferenceUtil.readGuestToken(app)
+                _loginGuestResult.value = Event(
+                    Result.Success(
+                        GuestSessionModel(
+                            guestSessionId = token, success = true, expiresAt = ""
+                        )
+                    )
+                )
+            }
+            _isLoading.value = false
         }
     }
 
+    private fun saveTokenAndExpiry(guestSession: GuestSessionModel) {
+        val expiredDate = guestSession.expiresAt.replace(" UTC", "", true)
+        val expiredDateInMillis = Util.convertStringTimeToMills(expiredDate)
+        PreferenceUtil.writeGuestToken(app, guestSession.guestSessionId)
+        PreferenceUtil.writeGuestTokenExpiry(app, expiredDateInMillis)
+    }
+
+    private fun isTokenValidAndAvailable(): Boolean {
+        val expireTime = PreferenceUtil.readGuestTokenExpiry(app)
+        val currentTime = Util.getCurrenTimeGMT()
+        return expireTime < 0 && currentTime > expireTime
+    }
+
     class Factory(
+        private val app: Application,
         private val remoteSource: RemoteServer
     ) : ViewModelProvider.Factory {
         @Suppress("UNCHECKED_CAST")
         override fun <T : ViewModel?> create(modelClass: Class<T>): T {
             if (modelClass.isAssignableFrom(AuthenticationViewModel::class.java)) {
-                return AuthenticationViewModel(remoteSource) as T
+                return AuthenticationViewModel(app, remoteSource) as T
             }
             throw IllegalArgumentException("Unknown ViewModel class")
         }
