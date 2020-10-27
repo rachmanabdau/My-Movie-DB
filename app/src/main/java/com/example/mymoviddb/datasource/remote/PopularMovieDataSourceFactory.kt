f64e5eedc050ccf3c9b883a7216575054c91a1e5
Rachman Arif Abda'u <rachmanabdau@gmail.com>
2020-10-27 18:31:32 +0700

    Create Popular Movie Data Source Factory

---
+++ app/src/main/java/com/example/mymoviddb/datasource/remote/PopularMovieDataSourceFactory.kt
@@ -0,0 +1,22 @@
+package com.example.mymoviddb.datasource.remote
+
+import androidx.lifecycle.MutableLiveData
+import androidx.paging.DataSource
+import com.example.mymoviddb.datasource.PopularMovieDataSource
+import com.example.mymoviddb.model.MovieModel
+import kotlinx.coroutines.CoroutineScope
+
+class PopularMovieDataSourceFactory(
+    private val networkService: RemoteServer,
+    private val scope: CoroutineScope
+) : DataSource.Factory<Int, MovieModel.Result>() {
+
+    val sourceLiveData = MutableLiveData<PopularMovieDataSource>()
+
+    override fun create(): DataSource<Int, MovieModel.Result> {
+        val source = PopularMovieDataSource(networkService, scope)
+        sourceLiveData.postValue(source)
+        return source
+    }
+
+}
