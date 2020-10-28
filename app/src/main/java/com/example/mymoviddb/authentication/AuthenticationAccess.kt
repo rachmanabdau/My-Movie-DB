846c646e80e1e7d19fce0f9d7e9188dce6ce9103
Rachman Arif Abda'u <rachmanabdau@gmail.com>
2020-10-28 09:49:45 +0700

    Implement IAuthenticationAccess

---
+++ app/src/main/java/com/example/mymoviddb/authentication/AuthenticationAccess.kt
@@ -0,0 +1,46 @@
+package com.example.mymoviddb.authentication
+
+import com.example.mymoviddb.datasource.remote.NetworkService
+import com.example.mymoviddb.model.GuestSessionModel
+import com.example.mymoviddb.model.LoginTokenModel
+import com.example.mymoviddb.model.RequestTokenModel
+import com.example.mymoviddb.model.Result
+import com.example.mymoviddb.utils.Util
+
+class AuthenticationAccess(private val access: NetworkService) : IAuthenticationAccess {
+
+    override suspend fun loginAsUser(
+        username: String,
+        password: String,
+        requestToken: RequestTokenModel?
+    ): Result<LoginTokenModel?> {
+        return try {
+            val result = access.loginAsync(
+                username, password, requestToken?.requestToken ?: ""
+            ).await()
+
+            if (result.isSuccessful && result.body() != null) {
+                Result.Success(result.body())
+            } else {
+                return Util.returnError(result)
+            }
+        } catch (e: Exception) {
+            return Result.Error(Exception(e.message))
+        }
+    }
+
+    override suspend fun loginAsGuest(apiKey: String): Result<GuestSessionModel?> {
+        return try {
+            val result = access.loginAsGuestAsync().await()
+
+            if (result.isSuccessful && result.body() != null) {
+                Result.Success(result.body())
+            } else {
+                return Util.returnError(result)
+            }
+        } catch (e: Exception) {
+            Result.Error(Exception(e.message))
+        }
+    }
+
+}
