2d24d4e53ae1337828a5e25455a1f0ace86cde84
Rachman Arif Abda'u <rachmanabdau@gmail.com>
2020-10-28 09:58:14 +0700

    Add Try-Catch in Category TV Show

--- app/src/main/java/com/example/mymoviddb/category/tv/CategoryTVListIAccess.kt
+++ app/src/main/java/com/example/mymoviddb/category/tv/CategoryTVListIAccess.kt
@@ -4,26 +4,34 @@ import com.example.mymoviddb.datasource.remote.NetworkService
 import com.example.mymoviddb.model.Result
 import com.example.mymoviddb.model.TVShowModel
 import com.example.mymoviddb.utils.Util
-
+//Add Try-Catch in Category Movie
 class CategoryTVListIAccess(private val access: NetworkService) : ICategoryTVListAccess {
 
     override suspend fun getPopularTvShowList(page: Int, apiKey: String): Result<TVShowModel?> {
-        val movieResult = access.getPopularTvShow(page, apiKey).await()
+        return try {
+            val movieResult = access.getPopularTvShow(page, apiKey).await()
 
-        return if (movieResult.isSuccessful) {
-            Result.Success(movieResult.body())
-        } else {
-            Util.returnError(movieResult)
+            if (movieResult.isSuccessful) {
+                Result.Success(movieResult.body())
+            } else {
+                Util.returnError(movieResult)
+            }
+        } catch (e: Exception) {
+            Result.Error(e)
         }
     }
 
     override suspend fun getOnAirTvShowList(page: Int, apiKey: String): Result<TVShowModel?> {
-        val movieResult = access.getOnAirTvShow(page, apiKey).await()
+        return try {
+            val movieResult = access.getOnAirTvShow(page, apiKey).await()
 
-        return if (movieResult.isSuccessful) {
-            Result.Success(movieResult.body())
-        } else {
-            Util.returnError(movieResult)
+            if (movieResult.isSuccessful) {
+                Result.Success(movieResult.body())
+            } else {
+                Util.returnError(movieResult)
+            }
+        } catch (e: Exception) {
+            Result.Error(e)
         }
     }
 }
