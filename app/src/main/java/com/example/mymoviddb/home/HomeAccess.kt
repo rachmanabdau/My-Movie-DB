fbd979cc6a9a7c8433318f41385766325df97599
Rachman Arif Abda'u <rachmanabdau@gmail.com>
2020-10-28 08:45:27 +0700

    Create Home Access

---
+++ app/src/main/java/com/example/mymoviddb/home/HomeAccess.kt
@@ -0,0 +1,51 @@
+package com.example.mymoviddb.home
+
+import com.example.mymoviddb.datasource.remote.NetworkService
+import com.example.mymoviddb.model.MovieModel
+import com.example.mymoviddb.model.Result
+import com.example.mymoviddb.model.TVShowModel
+import com.example.mymoviddb.utils.Util
+
+class HomeAccess(private val access: NetworkService) : HomeInterface {
+
+    override suspend fun getPopularMovieList(page: Int, apiKey: String): Result<MovieModel?> {
+        val result = access.getPopularMoviesAsync(page, apiKey).await()
+
+        return if (result.isSuccessful) {
+            Result.Success(result.body())
+        } else {
+            Util.returnError(result)
+        }
+    }
+
+    override suspend fun getNowPlayingMovieList(page: Int, apiKey: String): Result<MovieModel?> {
+        val result = access.getNowPlayingMoviesAsync(page, apiKey).await()
+
+        return if (result.isSuccessful) {
+            Result.Success(result.body())
+        } else {
+            Util.returnError(result)
+        }
+    }
+
+    override suspend fun getPopularTvShowList(page: Int, apiKey: String): Result<TVShowModel?> {
+        val result = access.getPopularTvShow(page, apiKey).await()
+
+        return if (result.isSuccessful) {
+            Result.Success(result.body())
+        } else {
+            Util.returnError(result)
+        }
+    }
+
+    override suspend fun getOnAirTvShowList(page: Int, apiKey: String): Result<TVShowModel?> {
+        val result = access.getOnAirTvShow(page, apiKey).await()
+
+        return if (result.isSuccessful) {
+            Result.Success(result.body())
+        } else {
+            Util.returnError(result)
+        }
+    }
+
+}
