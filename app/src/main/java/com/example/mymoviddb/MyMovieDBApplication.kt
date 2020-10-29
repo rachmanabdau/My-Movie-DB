6fdff3194d999155e1df9e2001a20c4d7b8377d3
Rachman Arif Abda'u <rachmanabdau@gmail.com>
2020-10-29 08:58:54 +0700

    Create Home Access Test

--- app/src/main/java/com/example/mymoviddb/MyMovieDBApplication.kt
+++ app/src/main/java/com/example/mymoviddb/MyMovieDBApplication.kt
@@ -1,9 +1,17 @@
 package com.example.mymoviddb
 
 import android.app.Application
+import com.example.mymoviddb.datasource.remote.NetworkAPI
+import com.example.mymoviddb.home.IHomeAccess
+import com.example.mymoviddb.home.ServiceLocatorHome
 import timber.log.Timber
 
 class MyMovieDBApplication : Application() {
+
+    val server = NetworkAPI.retrofitService
+
+    val homeAccess: IHomeAccess
+        get() = ServiceLocatorHome.provideHomeAccess(server)
 
     override fun onCreate() {
         super.onCreate()
