576a6da945a1e23654920bbc72b6930b76a6342d
Rachman Arif Abda'u <rachmanabdau@gmail.com>
2020-10-29 08:56:27 +0700

    Add Timber Logging

--- app/src/main/java/com/example/mymoviddb/MyMovieDBApplication.kt
+++ app/src/main/java/com/example/mymoviddb/MyMovieDBApplication.kt
@@ -1,6 +1,14 @@
 package com.example.mymoviddb
 
 import android.app.Application
+import timber.log.Timber
 
 class MyMovieDBApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
 }
