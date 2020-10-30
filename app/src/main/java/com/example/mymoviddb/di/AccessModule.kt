572523146811f3200981c0619c2cfc5dcc718103
Rachman Arif Abda'u <rachmanabdau@gmail.com>
2020-10-31 06:02:45 +0700

    Create DI for Home

--- app/src/main/java/com/example/mymoviddb/di/AccessModule.kt
+++ app/src/main/java/com/example/mymoviddb/di/AccessModule.kt
@@ -2,18 +2,20 @@ package com.example.mymoviddb.di
 
 import com.example.mymoviddb.authentication.AuthenticationAccess
 import com.example.mymoviddb.authentication.IAuthenticationAccess
+import com.example.mymoviddb.home.HomeAccess
+import com.example.mymoviddb.home.IHomeAccess
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.components.ActivityComponent
 
 @Module
-@InstallIn(ApplicationComponent::class)
+@InstallIn(ActivityComponent::class)
 abstract class AccessModule {
 
     @Binds
     abstract fun bindAuthAccess(authenticationAccess: AuthenticationAccess): IAuthenticationAccess
 
     @Binds
-    abstract fun bindHomeAccess(authenticationAccess: AuthenticationAccess): IAuthenticationAccess
+    abstract fun bindHomeAccess(authenticationAccess: HomeAccess): IHomeAccess
 }
