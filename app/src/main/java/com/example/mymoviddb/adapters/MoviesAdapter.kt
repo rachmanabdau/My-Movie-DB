95a9f457b698783a15b8e21702c3f3b6a69ecbb9
Rachman Arif Abda'u <rachmanabdau@gmail.com>
2020-10-31 17:36:53 +0700

    Update Movie Adapter to Use Load More View Holder

--- app/src/main/java/com/example/mymoviddb/adapters/MoviesAdapter.kt
+++ app/src/main/java/com/example/mymoviddb/adapters/MoviesAdapter.kt
@@ -1,16 +1,20 @@
 package com.example.mymoviddb.adapters
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.example.mymoviddb.databinding.LoadMoreItemBinding
 import com.example.mymoviddb.databinding.MovieItemBinding
 import com.example.mymoviddb.model.MovieModel
+import com.example.mymoviddb.utils.LoadMoreViewHolder
 
 class MoviesAdapter(private val action: () -> Unit) :
-    ListAdapter<MovieModel.Result, MovieViewHolder>(DiffUtilCallback) {
+    ListAdapter<MovieModel.Result, RecyclerView.ViewHolder>(DiffUtilCallback) {
+
+    private val loadMoreType = 0
+    private val movieType = 1
 
     companion object DiffUtilCallback : DiffUtil.ItemCallback<MovieModel.Result>() {
         override fun areItemsTheSame(
@@ -29,37 +33,40 @@ class MoviesAdapter(private val action: () -> Unit) :
 
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
-        val view = MovieItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return MovieViewHolder(view)
+    override fun getItemCount(): Int {
+        return if (super.getItemCount() > 0) super.getItemCount() + 1 else super.getItemCount()
     }
 
-    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
-        val data = getItem(position)
-        holder.onBind(data, position, itemCount, action)
+    override fun getItemViewType(position: Int): Int {
+        return if (position < itemCount - 1) movieType else loadMoreType
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return if (viewType == movieType) {
+            val view = MovieItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            MovieViewHolder(view)
+        } else {
+            val view =
+                LoadMoreItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            LoadMoreViewHolder(view)
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is MovieViewHolder && position < itemCount - 1) {
+            val data = getItem(position)
+            holder.onBind(data)
+        } else if (holder is LoadMoreViewHolder) {
+            holder.onBind(action)
+        }
     }
 }
 
 class MovieViewHolder(private val binding: MovieItemBinding) :
     RecyclerView.ViewHolder(binding.root) {
 
-    fun onBind(data: MovieModel.Result, position: Int, totalItem: Int, action: () -> Unit) {
+    fun onBind(data: MovieModel.Result/*, action: () -> Unit*/) {
         binding.popularMovie = data
         binding.rating = (data.voteAverage * 10).toInt()
-
-        if (position == totalItem - 1) {
-            binding.moviePoster.visibility = View.INVISIBLE
-            binding.movieTitleTv.visibility = View.INVISIBLE
-            binding.rateingString.visibility = View.INVISIBLE
-            binding.loadMore.visibility = View.VISIBLE
-            binding.loadMore.setOnClickListener {
-                action()
-            }
-        } else {
-            binding.moviePoster.visibility = View.VISIBLE
-            binding.movieTitleTv.visibility = View.VISIBLE
-            binding.rateingString.visibility = View.VISIBLE
-            binding.loadMore.visibility = View.GONE
-        }
     }
 }
