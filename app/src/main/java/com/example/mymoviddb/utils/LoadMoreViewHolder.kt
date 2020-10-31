5c136ea73534232bba96e33b38adb4b483e770bd
Rachman Arif Abda'u <rachmanabdau@gmail.com>
2020-10-31 17:36:19 +0700

    Create Load More View Holder

---
+++ app/src/main/java/com/example/mymoviddb/utils/LoadMoreViewHolder.kt
@@ -0,0 +1,14 @@
+package com.example.mymoviddb.utils
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mymoviddb.databinding.LoadMoreItemBinding
+
+class LoadMoreViewHolder(private val binding: LoadMoreItemBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+
+    fun onBind(action: () -> Unit) {
+        binding.loadmoreContainer.setOnClickListener {
+            action()
+        }
+    }
+}
