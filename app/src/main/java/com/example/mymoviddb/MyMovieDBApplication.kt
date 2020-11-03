051f40def1972b690dc34197fcc96b44586c4ce9
Rachman Arif Abda'u <rachmanabdau@gmail.com>
2020-11-03 09:23:10 +0700

    Delete Service Locator

--- app/src/main/java/com/example/mymoviddb/MyMovieDBApplication.kt
+++ app/src/main/java/com/example/mymoviddb/MyMovieDBApplication.kt
@@ -1,24 +1,11 @@
 package com.example.mymoviddb
 
 import android.app.Application
-import com.example.mymoviddb.category.movie.ICategoryMovieListAccess
-import com.example.mymoviddb.category.movie.ServiceLocatorCategoryMovie
-import com.example.mymoviddb.category.tv.ICategoryTVListAccess
-import com.example.mymoviddb.category.tv.ServiceLocatorCategoryTV
-import com.example.mymoviddb.datasource.remote.NetworkAPI
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
 @HiltAndroidApp
 class MyMovieDBApplication : Application() {
-
-    val server = NetworkAPI.retrofitService
-
-    val categoryMovieListAccess: ICategoryMovieListAccess
-        get() = ServiceLocatorCategoryMovie.provideServiceLocatorCategoryMovie(server)
-
-    val categoryTVListAccess: ICategoryTVListAccess
-        get() = ServiceLocatorCategoryTV.provideServiceLocatorCategoryTV(server)
 
     override fun onCreate() {
         super.onCreate()
