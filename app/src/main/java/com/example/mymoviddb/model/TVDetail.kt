20bbcf82d961452b354c02ff9dcd987cc7a1c323
Rachman Arif Abda'u <rachmanabdau@gmail.com>
2020-11-05 16:11:42 +0700

    Update TV Detail Model

--- app/src/main/java/com/example/mymoviddb/model/TVDetail.kt
+++ app/src/main/java/com/example/mymoviddb/model/TVDetail.kt
@@ -31,7 +31,7 @@ data class TVDetail(
     @Json(name = "networks")
     val networks: List<Network>,
     @Json(name = "next_episode_to_air")
-    val nextEpisodeToAir: Any,
+    val nextEpisodeToAir: NextEpisodeToAir?,
     @Json(name = "number_of_episodes")
     val numberOfEpisodes: Int,
     @Json(name = "number_of_seasons")
@@ -101,7 +101,7 @@ data class TVDetail(
         @Json(name = "still_path")
         val stillPath: String,
         @Json(name = "vote_average")
-        val voteAverage: Double,
+        val voteAverage: Int?,
         @Json(name = "vote_count")
         val voteCount: Int
     )
@@ -115,6 +115,31 @@ data class TVDetail(
         val name: String,
         @Json(name = "origin_country")
         val originCountry: String
+    )
+
+    data class NextEpisodeToAir(
+        @Json(name = "air_date")
+        val airDate: String,
+        @Json(name = "episode_number")
+        val episodeNumber: Int,
+        @Json(name = "id")
+        val id: Int,
+        @Json(name = "name")
+        val name: String,
+        @Json(name = "overview")
+        val overview: String,
+        @Json(name = "production_code")
+        val productionCode: String,
+        @Json(name = "season_number")
+        val seasonNumber: Int,
+        @Json(name = "show_id")
+        val showId: Int,
+        @Json(name = "still_path")
+        val stillPath: Any,
+        @Json(name = "vote_average")
+        val voteAverage: Int,
+        @Json(name = "vote_count")
+        val voteCount: Int
     )
 
     data class ProductionCompany(
