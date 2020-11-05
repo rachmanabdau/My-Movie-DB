1aa6bc41c6cd33cd74f26abca0fa1e175ddd2fbb
Rachman Arif Abda'u <rachmanabdau@gmail.com>
2020-11-06 05:20:10 +0700

    Create Method to call Recommendation and Similar Movies in View Model

--- app/src/main/java/com/example/mymoviddb/detail/DetailViewModel.kt
+++ app/src/main/java/com/example/mymoviddb/detail/DetailViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.mymoviddb.BuildConfig
 import com.example.mymoviddb.model.MovieDetail
+import com.example.mymoviddb.model.MovieModel
 import com.example.mymoviddb.model.Result
 import com.example.mymoviddb.model.TVDetail
 import kotlinx.coroutines.launch
@@ -16,6 +17,12 @@ class DetailViewModel @ViewModelInject constructor(private val detailaAccess: ID
 
     private val _movieDetail = MutableLiveData<Result<MovieDetail?>>()
     val movieDetail: LiveData<Result<MovieDetail?>> = _movieDetail
+
+    private val _recommendationMovies = MutableLiveData<Result<MovieModel?>>()
+    val recommendationMovies: LiveData<Result<MovieModel?>> = _recommendationMovies
+
+    private val _similarMovies = MutableLiveData<Result<MovieModel?>>()
+    val similarMovies: LiveData<Result<MovieModel?>> = _similarMovies
 
     private val _tvDetail = MutableLiveData<Result<TVDetail?>>()
     val tvDetail: LiveData<Result<TVDetail?>> = _tvDetail
@@ -29,6 +36,18 @@ class DetailViewModel @ViewModelInject constructor(private val detailaAccess: ID
     fun getTVDetail(tvId: Long, apiKey: String = BuildConfig.V3_AUTH) {
         viewModelScope.launch {
             _tvDetail.value = detailaAccess.getDetailTV(tvId, apiKey)
+        }
+    }
+
+    fun getRecommendationMovies(movieId: Long, apiKey: String = BuildConfig.V3_AUTH) {
+        viewModelScope.launch {
+            _recommendationMovies.value = detailaAccess.getRecommendationMovies(movieId, apiKey)
+        }
+    }
+
+    fun getSimilarMovies(movieId: Long, apiKey: String = BuildConfig.V3_AUTH) {
+        viewModelScope.launch {
+            _recommendationMovies.value = detailaAccess.getRecommendationMovies(movieId, apiKey)
         }
     }
 }
