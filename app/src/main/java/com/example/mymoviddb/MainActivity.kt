74f6c034753122d03cb9d9944012d67846654f9d
Rachman Arif Abda'u <rachmanabdau@gmail.com>
2020-11-06 20:46:21 +0700

    Add Drawer Menu

--- app/src/main/java/com/example/mymoviddb/MainActivity.kt
+++ app/src/main/java/com/example/mymoviddb/MainActivity.kt
@@ -3,9 +3,12 @@ package com.example.mymoviddb
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.example.mymoviddb.databinding.ActivityMainBinding
 import com.example.mymoviddb.utils.LoginState
@@ -35,6 +38,27 @@ class MainActivity : AppCompatActivity() {
             }
 
         navController.graph = graph
+
+        if (PreferenceUtil.getAuthState(this) == LoginState.AS_USER.ordinal) {
+            setupDrawerMenu(navController)
+        } else {
+            setupToolbarOnly(navController)
+        }
+    }
+
+    private fun setupDrawerMenu(navController: NavController) {
+        binding.drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+        val appbarConfig = AppBarConfiguration(navController.graph, binding.drawerLayout)
+        NavigationUI.setupWithNavController(
+            binding.mainToolbar.toolbar,
+            navController,
+            appbarConfig
+        )
+        binding.navView.setupWithNavController(navController)
+    }
+
+    private fun setupToolbarOnly(navController: NavController) {
+        binding.drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
         val appbarConfig = AppBarConfiguration(navController.graph)
         binding.mainToolbar.toolbar.setupWithNavController(navController, appbarConfig)
     }
