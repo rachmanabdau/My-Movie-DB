03684dc26673d069106ffd3b6a3f13352f744a6d
Rachman Arif Abda'u <rachmanabdau@gmail.com>
2020-11-06 22:08:25 +0700

    Recreate Activtiy When Login Succeed

--- app/src/main/java/com/example/mymoviddb/authentication/user/UserFragment.kt
+++ app/src/main/java/com/example/mymoviddb/authentication/user/UserFragment.kt
@@ -1,12 +1,13 @@
 package com.example.mymoviddb.authentication.user
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.findNavController
+import com.example.mymoviddb.MainActivity
 import com.example.mymoviddb.databinding.FragmentUserBinding
 import com.example.mymoviddb.utils.EventObserver
 import com.google.android.material.snackbar.Snackbar
@@ -37,9 +38,9 @@ class UserFragment : Fragment() {
 
         userViewModel.loginResult.observe(viewLifecycleOwner, EventObserver {
             if (it.equals("success", true)) {
-                findNavController().navigate(
-                    UserFragmentDirections.actionLoginFragmentToHomeFragment()
-                )
+                val intent = Intent(requireContext(), MainActivity::class.java)
+                startActivity(intent)
+                requireActivity().finish()
             } else {
                 Snackbar.make(requireView(), it, Snackbar.LENGTH_SHORT).show()
                 binding.login.isEnabled = true
