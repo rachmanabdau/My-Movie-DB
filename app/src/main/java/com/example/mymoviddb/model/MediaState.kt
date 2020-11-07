db0f70c1cc9418728532f441fe0d47d9bfe25686
Rachman Arif Abda'u <rachmanabdau@gmail.com>
2020-11-07 16:23:12 +0700

    Create Media State Model

---
+++ app/src/main/java/com/example/mymoviddb/model/MediaState.kt
@@ -0,0 +1,15 @@
+package com.example.mymoviddb.model
+
+
+import com.squareup.moshi.Json
+
+data class MediaState(
+    @Json(name = "favorite")
+    val favorite: Boolean,
+    @Json(name = "id")
+    val id: Int,
+    @Json(name = "rated")
+    val rated: Boolean,
+    @Json(name = "watchlist")
+    val watchlist: Boolean
+)
