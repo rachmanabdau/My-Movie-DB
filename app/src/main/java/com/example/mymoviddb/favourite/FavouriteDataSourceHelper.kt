e49bc7d0002d842fa622e4feec55ac7bc728040a
Rachman Arif Abda'u <rachmanabdau@gmail.com>
2020-11-11 17:51:42 +0700

    Create Favourite Datasource Helper

---
+++ app/src/main/java/com/example/mymoviddb/favourite/FavouriteDataSourceHelper.kt
@@ -0,0 +1,39 @@
+package com.example.mymoviddb.favourite
+
+import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
+import com.example.mymoviddb.model.Result
+import com.example.mymoviddb.model.ShowFavourite
+import kotlinx.coroutines.CoroutineScope
+
+class FavouriteDataSourceHelper(
+    app: Application,
+    networkService: IShowFavouriteAccess,
+    scope: CoroutineScope,
+    showType: Int
+) {
+    val favouriteMovieDataSourceFactory = FavouriteDatasourceFactory(
+        app,
+        networkService,
+        scope,
+        showType
+    )
+
+    fun getPageList(config: PagedList.Config): LiveData<PagedList<ShowFavourite.Result>> {
+        return LivePagedListBuilder(favouriteMovieDataSourceFactory, config).build()
+    }
+
+    fun getResult(): LiveData<Result<ShowFavourite?>> {
+        return Transformations.switchMap(
+            favouriteMovieDataSourceFactory.sourceLiveData,
+            FavouriteDatasource::result
+        )
+    }
+
+    fun getRetry() {
+        favouriteMovieDataSourceFactory.sourceLiveData.value?.retry()
+    }
+}
