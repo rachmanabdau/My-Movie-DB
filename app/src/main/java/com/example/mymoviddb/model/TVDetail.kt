2ccd55c4d9506ffc323a0242ac83fadbd41afa2a
Rachman Arif Abda'u <rachmanabdau@gmail.com>
2020-11-18 15:25:33 +0700

    Set Smoe Field to Nullable

--- app/src/main/java/com/example/mymoviddb/model/TVDetail.kt
+++ app/src/main/java/com/example/mymoviddb/model/TVDetail.kt
@@ -97,7 +97,7 @@ data class TVDetail(
         @Json(name = "season_number")
         val seasonNumber: Int,
         @Json(name = "show_id")
-        val showId: Int,
+        val showId: Int?,
         @Json(name = "still_path")
         val stillPath: String?,
         @Json(name = "vote_average")
@@ -125,7 +125,7 @@ data class TVDetail(
         @Json(name = "id")
         val id: Int,
         @Json(name = "name")
-        val name: String,
+        val name: String?,
         @Json(name = "overview")
         val overview: String,
         @Json(name = "production_code")
@@ -133,7 +133,7 @@ data class TVDetail(
         @Json(name = "season_number")
         val seasonNumber: Int,
         @Json(name = "show_id")
-        val showId: Int,
+        val showId: Int?,
         @Json(name = "still_path")
         val stillPath: String?,
         @Json(name = "vote_average")
