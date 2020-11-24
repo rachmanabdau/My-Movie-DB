d77299be1f4721d433e3c1d5c6cf89f6df129eb1
Rachman Arif Abda'u <rachmanabdau@gmail.com>
2020-11-24 21:55:46 +0700

    Implement Animation on Favourite Button

--- app/src/main/java/com/example/mymoviddb/detail/DetailActivity.kt
+++ app/src/main/java/com/example/mymoviddb/detail/DetailActivity.kt
@@ -1,11 +1,16 @@
 package com.example.mymoviddb.detail
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
+import android.widget.ImageView
 import androidx.activity.viewModels
+import androidx.annotation.ColorInt
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.navArgs
 import androidx.recyclerview.widget.RecyclerView
@@ -18,6 +23,7 @@ import com.example.mymoviddb.model.Result
 import com.example.mymoviddb.utils.EventObserver
 import com.example.mymoviddb.utils.LoginState
 import com.example.mymoviddb.utils.PreferenceUtil
+import com.example.mymoviddb.utils.Util.disableViewDuringAnimation
 import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -37,6 +43,8 @@ class DetailActivity : AppCompatActivity() {
     private val detailViewModel by viewModels<DetailViewModel>()
 
     private val args by navArgs<DetailActivityArgs>()
+
+    private var firstinitialize = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -58,7 +66,8 @@ class DetailActivity : AppCompatActivity() {
             intent.getLongExtra(SHOW_ID_KEY, 0L)
         })
 
-        detailViewModel.getShowDetail(binding.showId as Long, binding.showType as Int)
+        val sessionId = PreferenceUtil.readUserSession(this)
+        detailViewModel.getShowDetail(binding.showId as Long, binding.showType as Int, sessionId)
 
         // Show snack bar message from add/remove favourite (either succeed or failed)
         // Show snack bar message from add to/remove from watch list  (either succeed or failed)
@@ -66,29 +75,24 @@ class DetailActivity : AppCompatActivity() {
             Snackbar.make(binding.root, getString(it), Snackbar.LENGTH_SHORT).show()
         })
 
+        detailViewModel.isFavourited.observe(this) {
+            val colorTint =
+                if (it) ContextCompat.getColor(this, R.color.colorFavouriteActive)
+                else ContextCompat.getColor(this, R.color.colorBackgroound)
+
+            // Do not play animation at first intialize, just set the color filter without animation
+            if (!firstinitialize) {
+                setAnimationRotation(
+                    binding.favouriteBtnDetail, colorTint
+                )
+            } else {
+                binding.favouriteBtnDetail.setColorFilter(colorTint)
+            }
+        }
+
         setupToolbar()
         setupFAB(binding.showId ?: 0L, binding.showType as Int)
         loadData(binding.showId ?: 0L, binding.showType as Int)
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-        val userState = PreferenceUtil.getAuthState(this)
-
-        if (userState == LoginState.AS_USER.stateId) {
-            if (binding.showType as Int == DETAIL_MOVIE) {
-                val userSessionId = PreferenceUtil.readUserSession(this)
-                detailViewModel.getMovieAccountState(
-                    binding.showId ?: 0L, userSessionId
-                )
-            } else {
-                val userSessionId = PreferenceUtil.readUserSession(this)
-                detailViewModel.getTVAccountState(
-                    binding.showId ?: 0L, userSessionId
-                )
-            }
-        }
     }
 
     companion object {
@@ -114,12 +118,33 @@ class DetailActivity : AppCompatActivity() {
 
         // Button click listener for add to favourite
         binding.favouriteBtnDetail.setOnClickListener {
+            firstinitialize = false
             detailViewModel.markAsFavorite(userId, sessionId, showId, showType)
         }
 
         binding.addToWatchlistBtnDetail.setOnClickListener {
+            firstinitialize = false
             detailViewModel.addToWatchList(userId, sessionId, showId, showType)
         }
+    }
+
+    private fun setAnimationRotation(
+        imageView: ImageView,
+        @ColorInt colorChange: Int
+    ) {
+        val rotator = ObjectAnimator.ofFloat(imageView, View.ROTATION_Y, -720f, 0f)
+        rotator.disableViewDuringAnimation(imageView)
+
+        val colorizerActive =
+            ObjectAnimator.ofArgb(imageView, "colorFilter", colorChange)
+        colorizerActive.repeatCount = 0
+        colorizerActive.disableViewDuringAnimation(imageView)
+
+        val set = AnimatorSet()
+        set.playTogether(rotator, colorizerActive)
+        set.duration = 1000L
+
+        set.start()
     }
 
     private fun loadData(showId: Long, showType: Int) {
@@ -222,7 +247,8 @@ class DetailActivity : AppCompatActivity() {
             } else if (it is Result.Error) {
                 binding.errorDetail.errorMessage.text = it.exception.localizedMessage
                 binding.errorDetail.tryAgainButton.setOnClickListener {
-                    detailViewModel.getMovieDetail(showId)
+                    val sessionId = PreferenceUtil.readUserSession(this)
+                    detailViewModel.getMovieDetail(showId, sessionId)
                 }
             }
         }
@@ -241,6 +267,9 @@ class DetailActivity : AppCompatActivity() {
                     binding.errorRecommendationDetail.tryAgainButton.setOnClickListener {
                         detailViewModel.getRecommendationMovies(showId)
                     }
+                }
+
+                else -> {/* Do nothing set else just to avoid linter */
                 }
             }
 
@@ -270,6 +299,9 @@ class DetailActivity : AppCompatActivity() {
                         detailViewModel.getSimilarMovies(showId)
                     }
                 }
+
+                else -> {/* Do nothing set else just to avoid linter */
+                }
             }
 
             binding.errorRecommendationDetail.root.visibility =
@@ -297,7 +329,8 @@ class DetailActivity : AppCompatActivity() {
             } else if (it is Result.Error) {
                 binding.errorDetail.errorMessage.text = it.exception.localizedMessage
                 binding.errorDetail.tryAgainButton.setOnClickListener {
-                    detailViewModel.getTVDetail(showId)
+                    val sessionId = PreferenceUtil.readUserSession(this)
+                    detailViewModel.getTVDetail(showId, sessionId)
                 }
             }
         }
@@ -318,6 +351,9 @@ class DetailActivity : AppCompatActivity() {
                     binding.errorRecommendationDetail.tryAgainButton.setOnClickListener {
                         detailViewModel.getRecommendationTVShows(showId)
                     }
+                }
+
+                else -> {/* Do nothing set else just to avoid linter */
                 }
             }
 
@@ -346,6 +382,9 @@ class DetailActivity : AppCompatActivity() {
                     binding.errorSimilarDetail.tryAgainButton.setOnClickListener {
                         detailViewModel.getSimilarTVShows(showId)
                     }
+                }
+
+                else -> {/* Do nothing set else just to avoid linter */
                 }
             }
 
