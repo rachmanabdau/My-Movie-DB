28593befddfd10635accf7823a75020041a91304
Rachman Arif Abda'u <rachmanabdau@gmail.com>
2020-11-26 20:26:11 +0700

    Create Shimmer Place Holder

---
+++ app/src/main/java/com/example/mymoviddb/adapters/PlaceHolderAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.mymoviddb.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mymoviddb.databinding.LoadingPlaceholderItemBinding
+
+class PlaceHolderAdapter : RecyclerView.Adapter<DummyPlaceHolder>() {
+
+    val dummyList = mutableListOf<Int>().also { element ->
+        repeat(20) {
+            element.add(it)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DummyPlaceHolder {
+        val view = LoadingPlaceholderItemBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+        return DummyPlaceHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: DummyPlaceHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+        return dummyList.size
+    }
+}
+
+class DummyPlaceHolder(binding: LoadingPlaceholderItemBinding) :
+    RecyclerView.ViewHolder(binding.root)
