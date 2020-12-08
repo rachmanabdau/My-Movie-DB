118b70b8bdec0c1a1d3c183c11e180144647c398
Rachman Arif Abda'u <rachmanabdau@gmail.com>
2020-12-08 18:07:21 +0700

    Create TV Data Source V3

---
+++ app/src/main/java/com/example/mymoviddb/category/tv/TvDataSourceV3.kt
@@ -0,0 +1,69 @@
+package com.example.mymoviddb.category.tv
+
+import androidx.paging.PagingSource
+import com.example.mymoviddb.BuildConfig
+import com.example.mymoviddb.model.Result
+import com.example.mymoviddb.model.TVShowModel
+import com.example.mymoviddb.utils.wrapEspressoIdlingResource
+
+class TVDataSourceV3(
+    private val networkService: ICategoryTVListAccess,
+    private val categoryId: Int,
+    private val title: String
+) : PagingSource<Int, TVShowModel.Result>() {
+
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, TVShowModel.Result> {
+        wrapEspressoIdlingResource {
+            return try {
+                val pageNumber = params.key ?: 1
+                val tvShowResult = tvShowData(categoryId, pageNumber, networkService)
+
+                if (tvShowResult is Result.Success) {
+                    val tvShowList = tvShowResult.data?.results ?: emptyList()
+                    LoadResult.Page(data = tvShowList, prevKey = null, nextKey = pageNumber + 1)
+                } else {
+                    LoadResult.Error(getErrorMessage(tvShowResult))
+                }
+            } catch (e: Exception) {
+                LoadResult.Error(e)
+            }
+        }
+    }
+
+    companion object {
+        const val POPULAR_TV_ID = 1
+        const val ON_AIR_TV_ID = 2
+        const val SEARCH_TV = 32
+    }
+
+
+    private fun getErrorMessage(result: Result<*>): Exception {
+        return if (result is Result.Error) {
+            Exception(result.exception.localizedMessage ?: "Unknown exception occured.")
+        } else {
+            Exception("Unknown exception occured.")
+        }
+    }
+
+    private suspend fun tvShowData(
+        categoryId: Int,
+        pageNumber: Int,
+        networkService: ICategoryTVListAccess
+    ): Result<TVShowModel?> {
+        return when {
+            categoryId == SEARCH_TV && title.isNotBlank() -> {
+                networkService.searchTvShowList(title, pageNumber, BuildConfig.V3_AUTH)
+            }
+            categoryId == POPULAR_TV_ID -> {
+                networkService.getPopularTvShowList(pageNumber, BuildConfig.V3_AUTH)
+            }
+            categoryId == ON_AIR_TV_ID -> {
+                networkService.getOnAirTvShowList(pageNumber, BuildConfig.V3_AUTH)
+            }
+            else -> {
+                Result.Success(null)
+            }
+        }
+    }
+}
