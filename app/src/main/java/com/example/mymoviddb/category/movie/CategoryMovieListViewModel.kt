9bf0333d9a37b0cda71b98a4a5a4e5bd6e05b36f
Rachman Arif Abda'u <rachmanabdau@gmail.com>
2020-12-08 17:31:19 +0700

    Load Flowable Data Source to Live Data

--- app/src/main/java/com/example/mymoviddb/category/movie/CategoryMovieListViewModel.kt
+++ app/src/main/java/com/example/mymoviddb/category/movie/CategoryMovieListViewModel.kt
@@ -2,43 +2,35 @@ package com.example.mymoviddb.category.movie
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.Transformations
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.paging.LivePagedListBuilder
-import androidx.paging.PagedList
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import com.example.mymoviddb.model.MovieModel
-import com.example.mymoviddb.model.Result
-import kotlinx.coroutines.cancel
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
-class CategoryMovieListViewModel @ViewModelInject constructor(categoryMovieListAccess: ICategoryMovieListAccess) :
+class CategoryMovieListViewModel @ViewModelInject constructor(private val categoryMovieListAccess: ICategoryMovieListAccess) :
     ViewModel() {
 
-    val movieList: LiveData<PagedList<MovieModel.Result>>
-    val dataSourceFactory = MovieDataSourceFactory(categoryMovieListAccess, viewModelScope, "")
+    private val _moviePageData = MutableLiveData<PagingData<MovieModel.Result>>()
+    val moviePageData: LiveData<PagingData<MovieModel.Result>> = _moviePageData
 
-    init {
-        val config = PagedList.Config.Builder()
-            .setPageSize(20)
-            .setInitialLoadSizeHint(20)
-            .setPrefetchDistance(5)
-            .setEnablePlaceholders(true)
-            .build()
-
-        movieList = LivePagedListBuilder(dataSourceFactory, config).build()
-    }
-
-    val result: LiveData<Result<MovieModel?>> = Transformations.switchMap(
-        dataSourceFactory.sourceLiveData,
-        MovieDataSource::result
-    )
-
-    fun retry() {
-        dataSourceFactory.sourceLiveData.value?.retry()
-    }
-
-    override fun onCleared() {
-        super.onCleared()
-        viewModelScope.cancel()
+    fun getMovieData(/*categoryId: Int, title: String = ""*/) {
+        viewModelScope.launch {
+            Pager(
+                // Configure how data is loaded by passing additional properties to
+                // PagingConfig, such as prefetchDistance.
+                PagingConfig(pageSize = 20, prefetchDistance = 5)
+            ) {
+                MovieDataSourceV3(categoryMovieListAccess/*, categoryId, title*/)
+            }.flow
+                .cachedIn(this).collectLatest {
+                    _moviePageData.value = it
+                }
+        }
     }
 }
