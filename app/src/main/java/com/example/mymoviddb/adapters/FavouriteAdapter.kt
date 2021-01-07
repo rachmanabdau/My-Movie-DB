86b7641eda0bc92f675b5329fee20ae5c5cb75e6
Rachman Arif Abda'u <rachmanabdau@gmail.com>
2021-01-07 13:05:30 +0700

    Extends Favourite Adapter to PagingDataAdapter

--- app/src/main/java/com/example/mymoviddb/adapters/FavouriteAdapter.kt
+++ app/src/main/java/com/example/mymoviddb/adapters/FavouriteAdapter.kt
@@ -2,23 +2,15 @@ package com.example.mymoviddb.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.paging.PagedListAdapter
+import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil.ItemCallback
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mymoviddb.R
 import com.example.mymoviddb.databinding.FavouriteItemBinding
-import com.example.mymoviddb.databinding.TryAgainLoadListBinding
 import com.example.mymoviddb.model.FavouriteAndWatchListShow
-import com.example.mymoviddb.model.Result
-import com.example.mymoviddb.utils.ErrorViewHolder
 
-class FavouriteAdapter(private val retry: () -> Unit, private val actionDetail: (Long) -> Unit) :
-    PagedListAdapter<FavouriteAndWatchListShow.Result, RecyclerView.ViewHolder>(DiffUtil) {
-
-    private val typeError = 0
-    private val typeFavouriteShows = 1
-    private var state: Result<FavouriteAndWatchListShow?> = Result.Loading
-
+class FavouriteAdapter(private val actionDetail: (Long) -> Unit) :
+    PagingDataAdapter<FavouriteAndWatchListShow.Result, FavouriteViewHolder>(DiffUtil) {
 
     companion object DiffUtil : ItemCallback<FavouriteAndWatchListShow.Result>() {
         override fun areItemsTheSame(
@@ -37,45 +29,14 @@ class FavouriteAdapter(private val retry: () -> Unit, private val actionDetail: 
 
     }
 
-    override fun getItemViewType(position: Int): Int {
-        return if (position == itemCount - 1 && state !is Result.Success)
-            typeError
-        else typeFavouriteShows
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavouriteViewHolder {
+        val view = FavouriteItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return FavouriteViewHolder(view)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return when (viewType) {
-            typeFavouriteShows -> {
-                val view =
-                    FavouriteItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-                FavouriteViewHolder(view)
-            }
-            else -> {
-                val view =
-                    TryAgainLoadListBinding.inflate(
-                        LayoutInflater.from(parent.context),
-                        parent,
-                        false
-                    )
-                ErrorViewHolder(view)
-            }
-        }
-    }
-
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        if (holder is FavouriteViewHolder) {
-            val data = getItem(position)
-            holder.onBind(data, actionDetail)
-        } else if (holder is ErrorViewHolder && state !is Result.Success) {
-            val errorMessage =
-                if (state is Result.Error) (state as Result.Error).exception.localizedMessage else "Unknown error has occured"
-            holder.onBind(state, errorMessage) { retry() }
-        }
-    }
-
-    fun setState(state: Result<FavouriteAndWatchListShow?>) {
-        this.state = state
-        notifyDataSetChanged()
+    override fun onBindViewHolder(holder: FavouriteViewHolder, position: Int) {
+        val data = getItem(position)
+        holder.onBind(data, actionDetail)
     }
 
 }
@@ -88,7 +49,6 @@ class FavouriteViewHolder(private val binding: FavouriteItemBinding) :
             val context = binding.root.context
             binding.favouriteData = data
             binding.titleFavouriteItem.text = it.tvName ?: it.movieTitle
-
             binding.showPosterFavouriteItem.contentDescription = context.getString(
                 R.string.movie_content_description,
                 binding.titleFavouriteItem.text.toString()
